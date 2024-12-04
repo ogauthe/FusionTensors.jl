4cc3ccb1dd58c116a2382c8885013426fdff4386
Lukas Devos <ldevos98@gmail.com>
2024-12-04 08:20:00 -0500

    Fix Aqua tests and Project files (#3)
    * Fix Aqua tests and Project files
    
    * formatter

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -4,16 +4,7 @@ using Aqua: Aqua
 using Test: @testset
 
 @testset "Code quality (Aqua.jl)" begin
-  Aqua.test_ambiguities([FusionTensors])
-  Aqua.test_unbound_args(FusionTensors)
-  Aqua.test_undefined_exports(FusionTensors)
-  Aqua.test_project_extras(FusionTensors)
-  Aqua.test_deps_compat(FusionTensors)
-  Aqua.test_piracies(FusionTensors)
-  Aqua.test_persistent_tasks(FusionTensors)
-
-  # TODO fix test_stale_deps
-  # TODO replace with Aqua.test_all
-  # Aqua.test_stale_deps(FusionTensors)
+  Aqua.test_all(FusionTensors)
 end
+
 end
