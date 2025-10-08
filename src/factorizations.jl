a55f2543a88da83f875a9be12beaea5bea3787bd
Olivier Gauthé <ogauthe@flatironinstitute.org>
2025-10-08 17:55:35 +0200

    runic

--- src/factorizations.jl
+++ src/factorizations.jl
@@ -1,122 +1,122 @@
 using TensorAlgebra: TensorAlgebra, MatrixAlgebra
 using MatrixAlgebraKit:
-  MatrixAlgebraKit,
-  TruncationStrategy,
-  TruncationByOrder,
-  default_svd_algorithm,
-  findtruncated,
-  svd_compact!,
-  svd_trunc!
+    MatrixAlgebraKit,
+    TruncationStrategy,
+    TruncationByOrder,
+    default_svd_algorithm,
+    findtruncated,
+    svd_compact!,
+    svd_trunc!
 using GradedArrays:
-  GradedArrays,
-  BlockDiagonalAlgorithm,
-  eachblockaxis,
-  findfirstblock_sector,
-  isdual,
-  mortar_axis,
-  quantum_dimension,
-  sector
+    GradedArrays,
+    BlockDiagonalAlgorithm,
+    eachblockaxis,
+    findfirstblock_sector,
+    isdual,
+    mortar_axis,
+    quantum_dimension,
+    sector
 using BlockSparseArrays:
-  BlockPermutedDiagonalAlgorithm, blockdiagonalize, eachblockstoredindex
+    BlockPermutedDiagonalAlgorithm, blockdiagonalize, eachblockstoredindex
 using LinearAlgebra: LinearAlgebra, diag
 
 function MatrixAlgebraKit.default_svd_algorithm(::Type{<:FusionMatrix}; kwargs...)
-  return BlockPermutedDiagonalAlgorithm() do block
-    return default_svd_algorithm(block; kwargs...)
-  end
+    return BlockPermutedDiagonalAlgorithm() do block
+        return default_svd_algorithm(block; kwargs...)
+    end
 end
 
 function MatrixAlgebraKit.initialize_output(
-  ::typeof(svd_compact!), A::FusionMatrix, alg::BlockPermutedDiagonalAlgorithm
-)
-  # cannot be done in default_svd_algorithm which acts on type domain.
-  @assert !isdual(axes(A, 1)) && isdual(axes(A, 2))
+        ::typeof(svd_compact!), A::FusionMatrix, alg::BlockPermutedDiagonalAlgorithm
+    )
+    # cannot be done in default_svd_algorithm which acts on type domain.
+    @assert !isdual(axes(A, 1)) && isdual(axes(A, 2))
 
-  # we know initialized blocks are exactly invariant blocks
-  m = data_matrix(A)
-  s_axes = map(Iterators.flatten(eachblockstoredindex(m))) do b
-    return sector(axes(A, 1)[first(Tuple(b))]) => min(size(m[b])...)
-  end
-  s_axis = gradedrange(s_axes)
+    # we know initialized blocks are exactly invariant blocks
+    m = data_matrix(A)
+    s_axes = map(Iterators.flatten(eachblockstoredindex(m))) do b
+        return sector(axes(A, 1)[first(Tuple(b))]) => min(size(m[b])...)
+    end
+    s_axis = gradedrange(s_axes)
 
-  U = similar(A, eltype(A), ((axes(A, 1),), (dual(s_axis),)))
-  S = similar(A, real(eltype(A)), ((s_axis,), (dual(s_axis),)))
-  Vᴴ = similar(A, eltype(A), ((s_axis,), (axes(A, 2),)))
-  return U, S, Vᴴ
+    U = similar(A, eltype(A), ((axes(A, 1),), (dual(s_axis),)))
+    S = similar(A, real(eltype(A)), ((s_axis,), (dual(s_axis),)))
+    Vᴴ = similar(A, eltype(A), ((s_axis,), (axes(A, 2),)))
+    return U, S, Vᴴ
 end
 
 function BlockSparseArrays.blockdiagonalize(A::FusionMatrix)
-  m = data_matrix(A)
-  rowcolblocks = map(Iterators.flatten(eachblockstoredindex(m))) do b
-    return Tuple(b)
-  end
-  rowselect = first.(rowcolblocks)
-  colselect = last.(rowcolblocks)
-  return m[rowselect, colselect], (rowselect, colselect)
+    m = data_matrix(A)
+    rowcolblocks = map(Iterators.flatten(eachblockstoredindex(m))) do b
+        return Tuple(b)
+    end
+    rowselect = first.(rowcolblocks)
+    colselect = last.(rowcolblocks)
+    return m[rowselect, colselect], (rowselect, colselect)
 end
 
 function MatrixAlgebraKit.svd_compact!(
-  A::FusionMatrix, (U, S, Vᴴ), alg::BlockPermutedDiagonalAlgorithm
-)
-  Ad, (rowselect, colselect) = blockdiagonalize(A)
-  Um, Sm, Vᴴm = svd_compact!(Ad, BlockDiagonalAlgorithm(alg))
-  data_matrix(U)[rowselect, :] .= Um
-  data_matrix(S) .= Sm
-  data_matrix(Vᴴ)[:, colselect] .= Vᴴm
-  return U, S, Vᴴ
+        A::FusionMatrix, (U, S, Vᴴ), alg::BlockPermutedDiagonalAlgorithm
+    )
+    Ad, (rowselect, colselect) = blockdiagonalize(A)
+    Um, Sm, Vᴴm = svd_compact!(Ad, BlockDiagonalAlgorithm(alg))
+    data_matrix(U)[rowselect, :] .= Um
+    data_matrix(S) .= Sm
+    data_matrix(Vᴴ)[:, colselect] .= Vᴴm
+    return U, S, Vᴴ
 end
 
 function MatrixAlgebraKit.truncate!(
-  ::typeof(svd_trunc!),
-  (U, S, Vᴴ)::Tuple{FusionTensor,FusionTensor,FusionTensor},
-  strategy::TruncationStrategy,
-)
-  m = data_matrix(S)
-  g0 = codomain_axis(S)
-  vals = mapreduce(vcat, Iterators.flatten(eachblockstoredindex(m))) do b
-    b1 = first(Tuple(b))
-    d = quantum_dimension(sector(g0[b1]))
-    return Multiplet.(diag(m[b]), b1, d)
-  end
-  sort!(vals; rev=true)
-  indices_collection = findtruncated(vals, strategy)
-  kept_vals = vals[indices_collection]
-  kept_blocks = sort(unique(Block.(kept_vals)))
-  kept_sectors = map(b -> sector(g0[b]), kept_blocks)
-  # BlockIndexRange only allows contiguous ranges
-  kept_blockwise = [count(==(b), Block.(kept_vals)) for b in kept_blocks]
+        ::typeof(svd_trunc!),
+        (U, S, Vᴴ)::Tuple{FusionTensor, FusionTensor, FusionTensor},
+        strategy::TruncationStrategy,
+    )
+    m = data_matrix(S)
+    g0 = codomain_axis(S)
+    vals = mapreduce(vcat, Iterators.flatten(eachblockstoredindex(m))) do b
+        b1 = first(Tuple(b))
+        d = quantum_dimension(sector(g0[b1]))
+        return Multiplet.(diag(m[b]), b1, d)
+    end
+    sort!(vals; rev = true)
+    indices_collection = findtruncated(vals, strategy)
+    kept_vals = vals[indices_collection]
+    kept_blocks = sort(unique(Block.(kept_vals)))
+    kept_sectors = map(b -> sector(g0[b]), kept_blocks)
+    # BlockIndexRange only allows contiguous ranges
+    kept_blockwise = [count(==(b), Block.(kept_vals)) for b in kept_blocks]
 
-  g = gradedrange(kept_sectors .=> kept_blockwise)
-  Utrunc = similar(U, (codomain_axes(U), (dual(g),)))
-  Strunc = similar(S, ((g,), (dual(g),)))
-  Vᴴtrunc = similar(U, ((g,), domain_axes(Vᴴ)))
+    g = gradedrange(kept_sectors .=> kept_blockwise)
+    Utrunc = similar(U, (codomain_axes(U), (dual(g),)))
+    Strunc = similar(S, ((g,), (dual(g),)))
+    Vᴴtrunc = similar(U, ((g,), domain_axes(Vᴴ)))
 
-  # TBD: think in terms of block or of sectors?
-  # TODO use Dict{Sector,Vector{Int}} + dedicated function
-  for (i, b) in enumerate(kept_blocks)
-    r = Base.oneto(kept_blockwise[i])
-    bu = findfirstblock_sector(codomain_axis(U), kept_sectors[i])
-    bv = findfirstblock_sector(domain_axis(Vᴴ), kept_sectors[i])
-    data_matrix(Utrunc)[bu, Block(i)] .= data_matrix(U)[bu, b][:, r]
-    data_matrix(Strunc)[Block(i, i)] .= data_matrix(S)[b, b][r, r]
-    data_matrix(Vᴴtrunc)[Block(i), bv] .= data_matrix(Vᴴ)[bv, b][r, :]
-  end
+    # TBD: think in terms of block or of sectors?
+    # TODO use Dict{Sector,Vector{Int}} + dedicated function
+    for (i, b) in enumerate(kept_blocks)
+        r = Base.oneto(kept_blockwise[i])
+        bu = findfirstblock_sector(codomain_axis(U), kept_sectors[i])
+        bv = findfirstblock_sector(domain_axis(Vᴴ), kept_sectors[i])
+        data_matrix(Utrunc)[bu, Block(i)] .= data_matrix(U)[bu, b][:, r]
+        data_matrix(Strunc)[Block(i, i)] .= data_matrix(S)[b, b][r, r]
+        data_matrix(Vᴴtrunc)[Block(i), bv] .= data_matrix(Vᴴ)[bv, b][r, :]
+    end
 
-  return Utrunc, Strunc, Vᴴtrunc
+    return Utrunc, Strunc, Vᴴtrunc
 end
 
 # ==========================================================================================
 
 # TBD allow complex for truncated_eig?
 # TBD replace with (val, Sector) and use length(sector) internally?
-struct Multiplet{T<:Real,B,I}
-  val::T
-  blockval::B
-  quantum_dimension::I
+struct Multiplet{T <: Real, B, I}
+    val::T
+    blockval::B
+    quantum_dimension::I
 end
 
 Base.real(m::Multiplet) = m.val
-Base.real(::Type{M}) where {T,M<:Multiplet{T}} = T
+Base.real(::Type{M}) where {T, M <: Multiplet{T}} = T
 
 Base.isless(m1::Multiplet, m2::Multiplet) = real(m1) < real(m2)
 
@@ -126,15 +126,15 @@ BlockArrays.Block(m::Multiplet) = m.blockval
 GradedArrays.quantum_dimension(m::Multiplet) = m.quantum_dimension
 
 # TBD
-LinearAlgebra.norm(m::Multiplet, p::Real=2) = abs(real(m)) * (quantum_dimension(m)^(1 / p))
+LinearAlgebra.norm(m::Multiplet, p::Real = 2) = abs(real(m)) * (quantum_dimension(m)^(1 / p))
 
 function MatrixAlgebra.findtruncated(
-  vals::AbstractVector{<:Multiplet}, strategy::TruncationByOrder
-)
-  Base.require_one_based_indexing(vals)
-  issorted(vals; rev=true) || error("Not sorted.")
-  cs = cumsum(quantum_dimension.(vals))
-  k = findfirst(>(strategy.howmany), cs)
-  isnothing(k) && return Base.oneto(length(vals))
-  return Base.oneto(k - 1)  # remove last multiplet to have kept rank <= strategy.howmany
+        vals::AbstractVector{<:Multiplet}, strategy::TruncationByOrder
+    )
+    Base.require_one_based_indexing(vals)
+    issorted(vals; rev = true) || error("Not sorted.")
+    cs = cumsum(quantum_dimension.(vals))
+    k = findfirst(>(strategy.howmany), cs)
+    isnothing(k) && return Base.oneto(length(vals))
+    return Base.oneto(k - 1)  # remove last multiplet to have kept rank <= strategy.howmany
 end
--- test/test_factorizations.jl
+++ test/test_factorizations.jl
@@ -3,158 +3,158 @@ using TestExtras: @constinferred
 using LinearAlgebra: LinearAlgebra, isdiag
 using BlockSparseArrays: BlockSparseArray, eachstoredblock
 using FusionTensors:
-  FusionTensor, FusionTensorAxes, domain_axes, codomain_axes, to_fusiontensor
+    FusionTensor, FusionTensorAxes, domain_axes, codomain_axes, to_fusiontensor
 using GradedArrays:
-  SU2, U1, checkspaces, checkspaces_dual, dual, flip, gradedrange, isdual, space_isequal
+    SU2, U1, checkspaces, checkspaces_dual, dual, flip, gradedrange, isdual, space_isequal
 using TensorAlgebra: svd, tuplemortar
 using MatrixAlgebraKit: truncrank
 
 include("setup.jl")
 
 function check_svd(ft, u, s, v)
-  check_sanity(u)
-  check_sanity(s)
-  check_sanity(v)
-  @test u isa FusionTensor{eltype(ft),ndims_codomain(ft) + 1}
-  @test s isa FusionTensor{real(eltype(ft)),2}
-  @test !isdual(axes(s, 1))
-  @test isdual(axes(s, 2))
-  @test v isa FusionTensor{eltype(ft),ndims_domain(ft) + 1}
-  @test checkspaces(codomain_axes(ft), codomain_axes(u))
-  @test checkspaces_dual(domain_axes(u), codomain_axes(s))
-  @test checkspaces_dual(domain_axes(s), codomain_axes(s))
-  @test checkspaces_dual(domain_axes(s), codomain_axes(v))
-  @test checkspaces(domain_axes(v), domain_axes(ft))
+    check_sanity(u)
+    check_sanity(s)
+    check_sanity(v)
+    @test u isa FusionTensor{eltype(ft), ndims_codomain(ft) + 1}
+    @test s isa FusionTensor{real(eltype(ft)), 2}
+    @test !isdual(axes(s, 1))
+    @test isdual(axes(s, 2))
+    @test v isa FusionTensor{eltype(ft), ndims_domain(ft) + 1}
+    @test checkspaces(codomain_axes(ft), codomain_axes(u))
+    @test checkspaces_dual(domain_axes(u), codomain_axes(s))
+    @test checkspaces_dual(domain_axes(s), codomain_axes(s))
+    @test checkspaces_dual(domain_axes(s), codomain_axes(v))
+    @test checkspaces(domain_axes(v), domain_axes(ft))
 
-  foreach(eachstoredblock(data_matrix(s))) do b
-    @test isdiag(b)
-    @test all(b .>= 0)
-  end
-  foreach(eachstoredblock(data_matrix(u))) do b
-    @test b' * b ≈ LinearAlgebra.I(size(b, 2))
-  end
-  foreach(eachstoredblock(data_matrix(v))) do b
-    @test b * b' ≈ LinearAlgebra.I(size(b, 1))
-  end
-  return true
+    foreach(eachstoredblock(data_matrix(s))) do b
+        @test isdiag(b)
+        @test all(b .>= 0)
+    end
+    foreach(eachstoredblock(data_matrix(u))) do b
+        @test b' * b ≈ LinearAlgebra.I(size(b, 2))
+    end
+    foreach(eachstoredblock(data_matrix(v))) do b
+        @test b * b' ≈ LinearAlgebra.I(size(b, 1))
+    end
+    return true
 end
 
 function check_full_svd(ft, u, s, v)
-  check_svd(ft, u, s, v)
-  @test norm(s) ≈ norm(ft)
-  @test ft ≈ u * s * v
-  return true
+    check_svd(ft, u, s, v)
+    @test norm(s) ≈ norm(ft)
+    @test ft ≈ u * s * v
+    return true
 end
 
 @testset "full SVD (elt=$elt)" for elt in (Float64, ComplexF64)
-  # matrix
-  g1 = gradedrange([U1(1) => 1, U1(2) => 3])
-  g2 = gradedrange([U1(1) => 2, U1(2) => 2])
-  ft = randn(elt, FusionTensorAxes((g1,), (dual(g2),)))
-  u, s, v = @constinferred svd(ft, (1, 2), (1,), (2,))
-  @test check_full_svd(ft, u, s, v)
-  @test checkspaces(codomain_axes(s), (gradedrange([U1(1) => 1, U1(2) => 2]),))
+    # matrix
+    g1 = gradedrange([U1(1) => 1, U1(2) => 3])
+    g2 = gradedrange([U1(1) => 2, U1(2) => 2])
+    ft = randn(elt, FusionTensorAxes((g1,), (dual(g2),)))
+    u, s, v = @constinferred svd(ft, (1, 2), (1,), (2,))
+    @test check_full_svd(ft, u, s, v)
+    @test checkspaces(codomain_axes(s), (gradedrange([U1(1) => 1, U1(2) => 2]),))
 
-  # matrix with non canonical arrows
-  g1 = gradedrange([U1(1) => 1, U1(2) => 3])
-  ft = randn(FusionTensorAxes((dual(g1),), (flip(g2),)))
-  u, s, v = svd(ft, (1, 2), (1,), (2,))
-  @test check_full_svd(ft, u, s, v)
-  @test checkspaces(codomain_axes(s), (gradedrange([U1(-2) => 2, U1(-1) => 1]),))
+    # matrix with non canonical arrows
+    g1 = gradedrange([U1(1) => 1, U1(2) => 3])
+    ft = randn(FusionTensorAxes((dual(g1),), (flip(g2),)))
+    u, s, v = svd(ft, (1, 2), (1,), (2,))
+    @test check_full_svd(ft, u, s, v)
+    @test checkspaces(codomain_axes(s), (gradedrange([U1(-2) => 2, U1(-1) => 1]),))
 
-  # 4-dim tensor
-  g3 = gradedrange([U1(-1) => 1, U1(0) => 2, U1(1) => 1])
-  g4 = gradedrange([U1(-1) => 1, U1(0) => 1, U1(1) => 1])
-  ft = randn(FusionTensorAxes((g1, dual(g2)), (dual(g3), g4)))
-  u, s, v = @constinferred svd(ft, (1, 2, 3, 4), (1, 2), (3, 4))
-  @test check_full_svd(ft, u, s, v)
-  @test checkspaces(codomain_axes(s), (gradedrange([U1(-1) => 2, U1(0) => 4, U1(1) => 3]),))
+    # 4-dim tensor
+    g3 = gradedrange([U1(-1) => 1, U1(0) => 2, U1(1) => 1])
+    g4 = gradedrange([U1(-1) => 1, U1(0) => 1, U1(1) => 1])
+    ft = randn(FusionTensorAxes((g1, dual(g2)), (dual(g3), g4)))
+    u, s, v = @constinferred svd(ft, (1, 2, 3, 4), (1, 2), (3, 4))
+    @test check_full_svd(ft, u, s, v)
+    @test checkspaces(codomain_axes(s), (gradedrange([U1(-1) => 2, U1(0) => 4, U1(1) => 3]),))
 
-  # non-abelian
-  g2 = gradedrange([SU2(1//2) => 1])
-  sds22 = reshape(
-    [
-      [0.25, 0.0, 0.0, 0.0]
-      [0.0, -0.25, 0.5, 0.0]
-      [0.0, 0.5, -0.25, 0.0]
-      [0.0, 0.0, 0.0, 0.25]
-    ],
-    (2, 2, 2, 2),
-  )
-  ft = to_fusiontensor(sds22, (g2, g2), (dual(g2), dual(g2)))
-  u, s, v = @constinferred svd(ft, (1, 2, 3, 4), (1, 2), (3, 4))
-  @test check_full_svd(ft, u, s, v)
-  @test checkspaces(codomain_axes(s), (gradedrange([SU2(0) => 1, SU2(1) => 1]),))
-  @test data_matrix(s) ≈ [3/4 0; 0 1/4]
+    # non-abelian
+    g2 = gradedrange([SU2(1 // 2) => 1])
+    sds22 = reshape(
+        [
+            [0.25, 0.0, 0.0, 0.0]
+            [0.0, -0.25, 0.5, 0.0]
+            [0.0, 0.5, -0.25, 0.0]
+            [0.0, 0.0, 0.0, 0.25]
+        ],
+        (2, 2, 2, 2),
+    )
+    ft = to_fusiontensor(sds22, (g2, g2), (dual(g2), dual(g2)))
+    u, s, v = @constinferred svd(ft, (1, 2, 3, 4), (1, 2), (3, 4))
+    @test check_full_svd(ft, u, s, v)
+    @test checkspaces(codomain_axes(s), (gradedrange([SU2(0) => 1, SU2(1) => 1]),))
+    @test data_matrix(s) ≈ [3 / 4 0; 0 1 / 4]
 end
 
 @testset "Abelian truncated SVD (elt=$elt)" for elt in (Float64, ComplexF64)
-  # only test matrices; matricize handles higer dimension tensors
-  g1 = gradedrange([U1(1) => 5, U1(2) => 6])
-  g2 = gradedrange([U1(1) => 8, U1(2) => 4])
-  ft = randn(elt, FusionTensorAxes((g1,), (dual(g2),)))
+    # only test matrices; matricize handles higer dimension tensors
+    g1 = gradedrange([U1(1) => 5, U1(2) => 6])
+    g2 = gradedrange([U1(1) => 8, U1(2) => 4])
+    ft = randn(elt, FusionTensorAxes((g1,), (dual(g2),)))
 
-  trunc = truncrank(5)
+    trunc = truncrank(5)
 
-  u, s, v = svd(ft, (1, 2), (1,), (2,))
-  utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc)
-  @test check_svd(ft, utrunc, strunc, vtrunc)
-  @test size(strunc) == tuplemortar(((5,), (5,)))
-  ft_trunc = utrunc * strunc * vtrunc
-  # TBD check norm(ft-ft_trunc)?
+    u, s, v = svd(ft, (1, 2), (1,), (2,))
+    utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc)
+    @test check_svd(ft, utrunc, strunc, vtrunc)
+    @test size(strunc) == tuplemortar(((5,), (5,)))
+    ft_trunc = utrunc * strunc * vtrunc
+    # TBD check norm(ft-ft_trunc)?
 
-  # trunc above total number of values
-  utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc=truncrank(100))
-  @test check_full_svd(ft, utrunc, strunc, vtrunc)
+    # trunc above total number of values
+    utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc = truncrank(100))
+    @test check_full_svd(ft, utrunc, strunc, vtrunc)
 
-  # keep 0 values
-  utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc=truncrank(0))
-  # TBD fix isdual(length = 0) or not worth it?
-  #@test check_svd(ft, utrunc, strunc, vtrunc)
-  @test size(strunc) == tuplemortar(((0,), (0,)))
+    # keep 0 values
+    utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc = truncrank(0))
+    # TBD fix isdual(length = 0) or not worth it?
+    #@test check_svd(ft, utrunc, strunc, vtrunc)
+    @test size(strunc) == tuplemortar(((0,), (0,)))
 
-  # add forbidden blocks
-  g1 = gradedrange([U1(0) => 2, U1(1) => 5, U1(2) => 6])
-  g2 = gradedrange([U1(1) => 8, U1(2) => 4, U1(4) => 3])
-  ft = randn(FusionTensorAxes((g1,), (dual(g2),)))
-  u, s, v = svd(ft, (1, 2), (1,), (2,))
-  utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc)
-  @test check_svd(ft, utrunc, strunc, vtrunc)
-  @test size(strunc) == tuplemortar(((5,), (5,)))
-  ft_trunc = utrunc * strunc * vtrunc
+    # add forbidden blocks
+    g1 = gradedrange([U1(0) => 2, U1(1) => 5, U1(2) => 6])
+    g2 = gradedrange([U1(1) => 8, U1(2) => 4, U1(4) => 3])
+    ft = randn(FusionTensorAxes((g1,), (dual(g2),)))
+    u, s, v = svd(ft, (1, 2), (1,), (2,))
+    utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc)
+    @test check_svd(ft, utrunc, strunc, vtrunc)
+    @test size(strunc) == tuplemortar(((5,), (5,)))
+    ft_trunc = utrunc * strunc * vtrunc
 end
 
 random_unitary(t::Tuple) = random_unitary(t...)
 random_unitary(m, n) = Matrix(LinearAlgebra.qr(randn(m, n)).Q)
 
 @testset "Non-abelian truncated SVD" begin
-  g1 = gradedrange([SU2(0) => 1, SU2(1//2) => 6, SU2(1) => 8])
-  g2 = gradedrange([SU2(1//2) => 5, SU2(1) => 6, SU2(3//2) => 1])
-  ft = zeros(FusionTensorAxes((g1,), (dual(g2),)))
+    g1 = gradedrange([SU2(0) => 1, SU2(1 // 2) => 6, SU2(1) => 8])
+    g2 = gradedrange([SU2(1 // 2) => 5, SU2(1) => 6, SU2(3 // 2) => 1])
+    ft = zeros(FusionTensorAxes((g1,), (dual(g2),)))
 
-  u1 = random_unitary(6, 5)
-  v1 = random_unitary(5, 5)
-  s1 = LinearAlgebra.diagm(1:5)
-  u2 = random_unitary(8, 6)
-  v2 = random_unitary(6, 6)
-  s2 = LinearAlgebra.diagm(6:11)
-  data_matrix(ft)[Block(2, 1)] .= u1 * s1 * v1
-  data_matrix(ft)[Block(3, 2)] .= u2 * s2 * v2
+    u1 = random_unitary(6, 5)
+    v1 = random_unitary(5, 5)
+    s1 = LinearAlgebra.diagm(1:5)
+    u2 = random_unitary(8, 6)
+    v2 = random_unitary(6, 6)
+    s2 = LinearAlgebra.diagm(6:11)
+    data_matrix(ft)[Block(2, 1)] .= u1 * s1 * v1
+    data_matrix(ft)[Block(3, 2)] .= u2 * s2 * v2
 
-  u, s, v = svd(ft, (1, 2), (1,), (2,))
-  utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc=truncrank(5))
-  @test check_svd(ft, utrunc, strunc, vtrunc)
-  @test size(strunc) == tuplemortar(((3,), (3,)))
-  @test space_isequal(axes(strunc, 1), gradedrange([SU2(1) => 1]))
+    u, s, v = svd(ft, (1, 2), (1,), (2,))
+    utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc = truncrank(5))
+    @test check_svd(ft, utrunc, strunc, vtrunc)
+    @test size(strunc) == tuplemortar(((3,), (3,)))
+    @test space_isequal(axes(strunc, 1), gradedrange([SU2(1) => 1]))
 
-  utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc=truncrank(6))
-  @test check_svd(ft, utrunc, strunc, vtrunc)
-  @test size(strunc) == tuplemortar(((6,), (6,)))
-  @test space_isequal(axes(strunc, 1), gradedrange([SU2(1) => 2]))
-  ft_trunc = utrunc * strunc * vtrunc
-  # TBD check norm(ft-ft_trunc)?
+    utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc = truncrank(6))
+    @test check_svd(ft, utrunc, strunc, vtrunc)
+    @test size(strunc) == tuplemortar(((6,), (6,)))
+    @test space_isequal(axes(strunc, 1), gradedrange([SU2(1) => 2]))
+    ft_trunc = utrunc * strunc * vtrunc
+    # TBD check norm(ft-ft_trunc)?
 
-  # trunc above total number of values
-  utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc=truncrank(500))
-  @test check_full_svd(ft, utrunc, strunc, vtrunc)
+    # trunc above total number of values
+    utrunc, strunc, vtrunc = svd(ft, (1, 2), (1,), (2,); trunc = truncrank(500))
+    @test check_full_svd(ft, utrunc, strunc, vtrunc)
 end
